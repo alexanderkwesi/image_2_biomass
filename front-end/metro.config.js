01297092db78e87277f5afa5fcaeb04349d98d74
Alexander Kwesi <seankwesi24@googlemail.com>
2026-07-18 19:38:19 +0100

    Create metro.config.js

---
+++ front-end/metro.config.js
@@ -0,0 +1,20 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.transformer = {
+  ...config.transformer,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
+};
+
+config.resolver = {
+  ...config.resolver,
+  sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'mjs'],
+};
+
+module.exports = config;
